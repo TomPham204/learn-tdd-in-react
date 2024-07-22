7dbf4a477a4dd285f6e27a2ad5eb62fa096fdf9f
TomPham204 <thect04@gmail.com>
2024-07-22 11:20:52 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import NewMessageForm from "./NewMessageForm";
+import MessageList from "./MessageList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const [messages, setMessages] = useState([]);
+
+	function handleSend() {
+		setMessages([...messages, "New message"]);
+	}
+
+	return (
+		<>
+			<NewMessageForm onSend={handleSend} />
+			<MessageList data={messages} />
+		</>
+	);
 }
-
-export default App;
